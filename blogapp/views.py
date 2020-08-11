c60f8ce4dcae0964a48e25ebb483ce64e4caaac2
stackpython <stackpython2019@gmail.com>
2020-08-11 23:02:39 +0700

    Register app's urls to main urls

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog
+
 
 # Create your views here.
+def home(request):
+
+    return HttpResponse("Hello, Django")
+
+
+    
+
+
+
