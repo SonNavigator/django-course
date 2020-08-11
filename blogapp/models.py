91e91c363ef1ef7c7802585c407e69e5fc8b305d
stackpython <stackpython2019@gmail.com>
2020-08-11 16:03:38 +0700

    first commit

---
+++ blogapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=120)
+    post = models.TextField()
+    date_created = models.DateTimeField()
+    date_updated = models.DateTimeField()
+
+    
